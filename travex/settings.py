9d559548af3b5a4ef5c308915d83c863bfa84a29
Perman <pera121101@gmail.com>
2022-03-15 22:01:47 +0500

    dobawil password dlya postgresql

--- travex/settings.py
+++ travex/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'travex_db',
         'USER': 'travex_user',
-        'PASSWORD': '',
+        'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
