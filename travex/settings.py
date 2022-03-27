e53a6b0df12ec567a434b222c4ec2000c21c01ac
Perman <pera121101@gmail.com>
2022-03-27 13:16:16 +0500

    dobawil ALLOWED_HOSTS port

--- travex/settings.py
+++ travex/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-oz9%=u1yzyqxsg92!12104&)zl1$%-xrpeez)9xiv*%s8fzr^!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.67.98.251', '127.0.0.1']
+ALLOWED_HOSTS = ['3.67.98.251', '127.0.0.1', '159.223.216.170']
 
 
 # Application definition
