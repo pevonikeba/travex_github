33c971f32f15e090a4a98d9ffb5acbb4e7d1df9e
Perman <pera121101@gmail.com>
2022-04-07 15:32:39 +0500

    is_active = false

--- place/views.py
+++ place/views.py
@@ -64,6 +64,12 @@ class BookmarkViewSet(ModelViewSet):
     serializer_class = BookmarkSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class GroupViewSet(ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
