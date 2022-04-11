4786dc12ad9ee44962c07944586b05df8b1573f1
Perman <pera121101@gmail.com>
2022-04-11 22:24:04 +0500

    access token liftime 1 year

--- travex/settings.py
+++ travex/settings.py
@@ -258,7 +258,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=525600),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
