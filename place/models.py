0155229cbc2a672480392cbeb8305ee4aafebc86
Perman <pera121101@gmail.com>
2022-04-12 15:45:39 +0500

    ubral geo

--- place/models.py
+++ place/models.py
@@ -264,20 +264,20 @@ class UserPlaceRelation(models.Model):
 #         return self.name
 
 # Auto-generated `LayerMapping` dictionary for WorldBorder model
-worldborder_mapping = {
-    'fips': 'FIPS',
-    'iso2': 'ISO2',
-    'iso3': 'ISO3',
-    'un': 'UN',
-    'name': 'NAME',
-    'area': 'AREA',
-    'pop2005': 'POP2005',
-    'region': 'REGION',
-    'subregion': 'SUBREGION',
-    'lon': 'LON',
-    'lat': 'LAT',
-    'geom': 'MULTIPOLYGON',
-}
+# worldborder_mapping = {
+#     'fips': 'FIPS',
+#     'iso2': 'ISO2',
+#     'iso3': 'ISO3',
+#     'un': 'UN',
+#     'name': 'NAME',
+#     'area': 'AREA',
+#     'pop2005': 'POP2005',
+#     'region': 'REGION',
+#     'subregion': 'SUBREGION',
+#     'lon': 'LON',
+#     'lat': 'LAT',
+#     'geom': 'MULTIPOLYGON',
+# }
 
 
 class Location(models.Model):
