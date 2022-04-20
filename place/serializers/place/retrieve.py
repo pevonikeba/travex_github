c88ee0ff02055864699d2ed8264b8edc2659269d
arslion-7 <arslion@yandex.ru>
2022-04-20 21:37:29 +0500

    Temporaly restore retrieve method in place view set

--- place/serializers/place/retrieve.py
+++ place/serializers/place/retrieve.py
@@ -3,123 +3,131 @@ from rest_framework import serializers
 
 from place.models import Place, Image
 
+
 {
 "sections": [
                 {
                     "children": [
                         {
                             "id": 1,
-                            "description": "<p>Continent: asia</p><p>Country: JP</p><p>region: region</p><p>City: city</p><p>Latitude: 24.2500000000</p><p>Longitude: 23.2300000000</p><p>nearest_place: nearest place</p>"
+                            "title": "",
+                            "description": "<p>Continent: asia</p><p>Country: Nigeria</p><p>region: Kebbi</p><p>City: Waje</p><p>Latitude: 11.5516225000</p><p>Longitude: 5.4981583000</p><p>nearest_place: nearest place</p>"
                         }
                     ],
-                    "title": "Locations",
-                    "key": "locations",
+                    "title": "Location",
+                    "key": "location",
+                    "icon_name": "article",
                     "display_type": "drop_down"
                 },
                 {
                     "children": [
                         {
                             "id": 1,
-                            "description": "<h1>type name</h1><img src=/media/images/authentic.webp/><p>Price: 3245.00</p><p>Comfortable: comfortable</p><p>asdgasdgasdg sd fsad a</p>"
+                            "title": "<h1>type name</h1>",
+                            "description": "<img src=/media/images/authentic.webp/><p>Price: 3245.00</p><p>Comfortable: comfortable</p><p>asdgasdgasdg sd fsad a</p>"
                         }
                     ],
-                    "title": "Transports",
-                    "key": "transports",
+                    "title": "Transport",
+                    "key": "transport",
+                    "icon_name": "article",
                     "display_type": "drop_down"
                 },
                 {
                     "children": [
                         {
                             "id": 1,
-                            "description": "<h1>sdgasg</h1><p>Price: 10.00</p><p>sklahguhs</p>"
+                            "title": "<h1>sdgasg</h1>",
+                            "description": "<p>Price: 10.00</p><p>sklahguhs</p>"
                         }
                     ],
-                    "title": "Accommodationoptions",
-                    "key": "accommodationoptions",
+                    "title": "Accommodation option",
+                    "key": "accommodation_option",
+                    "icon_name": "article",
                     "display_type": "drop_down"
                 },
                 {
                     "children": [
                         {
                             "id": 1,
-                            "description": "<h1>sdgas</h1><img src=/media/images/medical_and_health_miracle.jpeg/><p>ashasd asgas dg asdhas h ash</p>"
+                            "title": "<h1>sdgas</h1>",
+                            "description": "<img src=/media/images/medical_and_health_miracle.jpeg/><p>ashasd asgas dg asdhas h ash</p>"
                         }
                     ],
                     "title": "Must see",
                     "key": "must_see",
+                    "icon_name": "article",
                     "display_type": "drop_down"
                 },
                 {
                     "children": [
                         {
                             "id": 1,
-                            "description": "<h1>sdgs sd</h1><img src=/media/images/lafoten_tukan.jpeg/><p>g sag as gsd ga</p>"
+                            "title": "<h1>sdgs sd</h1>",
+                            "description": "<img src=/media/images/lafoten_tukan.jpeg/><p>g sag as gsd ga</p>"
                         }
                     ],
                     "title": "Where to take a picture",
                     "key": "where_to_take_a_picture",
+                    "icon_name": "article",
                     "display_type": "drop_down"
                 },
                 {
                     "children": [
                         {
                             "id": 1,
-                            "description": "<h1>asdg</h1><img src=/media/images/culture_historical__kslnLE2.jpeg/><p>Price: 324.00</p><p>asdgksdjgj as</p>"
+                            "title": "<h1>asdg</h1>",
+                            "description": "<img src=/media/images/culture_historical__kslnLE2.jpeg/><p>Price: 324.00</p><p>asdgksdjgj as</p>"
                         }
                     ],
-                    "title": "Cuisines",
-                    "key": "cuisines",
+                    "title": "Cuisine",
+                    "key": "cuisine",
+                    "icon_name": "article",
                     "display_type": "drop_down"
                 },
                 {
                     "children": [
                         {
                             "id": 1,
-                            "description": "<h1>safe name</h1><p>How Dangerous: somewhat dangerous</p><p>Rating Danger: 4</p><p>safe description</p>"
-                        }
-                    ],
-                    "title": "Safes",
-                    "key": "safes",
-                    "display_type": "drop_down"
-                },
-                {
-                    "children": [
-                        {
-                            "id": 1,
-                            "description": "<h1>gsdgasdg s</h1><img src=/media/images/sports_and_extreme_recreation.jpeg/><p>jh dslsh dhf as</p>"
+                            "title": "<h1>gsdgasdg s</h1>",
+                            "description": "<img src=/media/images/sports_and_extreme_recreation.jpeg/><p>jh dslsh dhf as</p>"
                         }
                     ],
                     "title": "Natural phenomena",
                     "key": "natural_phenomena",
+                    "icon_name": "article",
                     "display_type": "drop_down"
                 },
                 {
-                    "vibe": "sdgas",
                     "children": [
                         {
                             "id": 1,
+                            "title": "<h1>sdgas</h1>",
                             "description": "<img src=/media/images/pilgrimage.webp/>"
                         }
                     ],
-                    "title": "Vibes",
-                    "key": "vibes",
+                    "title": "Vibe",
+                    "key": "vibe",
+                    "icon_name": "article",
                     "display_type": "drop_down"
                 },
                 {
                     "children": [
                         {
                             "id": 1,
+                            "title": "",
                             "description": "<img src=/media/images/wise_space_to_philosophize.jpeg/><p>skahslkjd sdf asjf;laks</p>"
                         }
                     ],
-                    "title": "Interesting facts",
-                    "key": "interesting_facts",
+                    "title": "Interesting fact",
+                    "key": "interesting_fact",
+                    "icon_name": "article",
                     "display_type": "drop_down"
                 },
                 {
                     "children": [
                         {
                             "id": 1,
+                            "title": "",
                             "description": "<p>sdgjshd ljfhasjdhfl asdhfjas fa</p>"
                         },
                         {
@@ -131,15 +139,17 @@ from place.models import Place, Image
                             "description": "<p>asdjfkljsad;lfj asmkldf</p>"
                         }
                     ],
-                    "title": "Practical informations",
-                    "key": "practical_informations",
+                    "title": "Practical information",
+                    "key": "practical_information",
+                    "icon_name": "article",
                     "display_type": "drop_down"
                 },
                 {
                     "children": [
                         {
                             "id": 1,
-                            "description": "<h1>lahs udhgf iuashd fkjsa dfk a</h1><img src=/media/images/adventure_spirit_SUDzMPj.jpeg/><p>h asljd hfashdfkhasdfjalsjdf ashdfjasdf kas;dfk a</p>"
+                            "title": "<h1>lahs udhgf iuashd fkjsa dfk a</h1>",
+                            "description": "<img src=/media/images/adventure_spirit_SUDzMPj.jpeg/><p>h asljd hfashdfkhasdfjalsjdf ashdfjasdf kas;dfk a</p>"
                         },
                         {
                             "id": 2,
@@ -148,13 +158,14 @@ from place.models import Place, Image
                     ],
                     "title": "Flora fauna",
                     "key": "flora_fauna",
+                    "icon_name": "article",
                     "display_type": "grid"
                 }
             ]
 }
 
+
 class ImageSerializer(serializers.ModelSerializer):
-    # path = Base64ImageField()  # From DRF Extra Fields
     class Meta:
         model = Image
         fields = ('id', 'path',)
@@ -169,7 +180,20 @@ class PlaceRetrieveSerializer(serializers.ModelSerializer):
         fields = ('id', 'images', 'rating', 'location', 'writer_user', 'sections',)
         # depth = 1
 
-    # def create
+    def create_section(self):
+        return {
+                "children": [
+                    {
+                        "id": 1,
+                        "title": "",
+                        "description": "<p>Continent: asia</p><p>Country: Nigeria</p><p>region: Kebbi</p><p>City: Waje</p><p>Latitude: 11.5516225000</p><p>Longitude: 5.4981583000</p><p>nearest_place: nearest place</p>"
+                    }
+                ],
+                "title": "Location",
+                "key": "location",
+                "icon_name": "article",
+                "display_type": "drop_down"
+            }
 
     def get_sections(self, obj):
         return [
