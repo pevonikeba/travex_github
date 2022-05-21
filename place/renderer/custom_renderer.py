9169277fb2125bb87b81b84e0a4fee2e7c74f4ae
arslion-7 <arslion@yandex.ru>
2022-05-21 21:55:27 +0500

    detail in auth error catched

--- place/renderer/custom_renderer.py
+++ place/renderer/custom_renderer.py
@@ -1,21 +1,27 @@
 from loguru import logger
 from rest_framework.renderers import JSONRenderer
+import enum
+
+
+class FullPath(enum.Enum):
+    reset_password = '/auth/users/reset_password/'
+
+# logger.warning(renderer_context.get('request').get_full_path())
 
 
 class CustomRenderer(JSONRenderer):
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        logger.warning(data)
         response_content = {}
-        # logger.info('here')
+        logger.info('here')
         if isinstance(data, dict) and data.get('status_code'):
             response_content['success'] = False
-            logger.info(dir(data['detail']))
-            logger.warning(data['detail'].title())
-            if data['detail'].title() == 'Invalid Username/Password.':
-                error = 'authentication_failed'
-            else:
-                error = data['token_error'] or data or 'unknown_error'
+            detail = data.get('detail')
+            if detail:
+                if detail.title() == 'Invalid Username/Password.':
+                    error = 'authentication_failed'
+                else:
+                    error = data['token_error'] or data or 'unknown_error'
             response_content['error'] = error
             # response_content['message'] = data
             # response_content['token_error'] = or 'unknown_error'
