a7414e24e826cd1abea88ba50f5a518dd5b5c412
arslion-7 <arslion@yandex.ru>
2022-05-22 14:04:43 +0500

    socialaccount adapter added to settings

--- travex/adapter.py
+++ travex/adapter.py
@@ -1,14 +1,20 @@
-# from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-#
-#
-# class MySocialAccountAdapter(DefaultSocialAccountAdapter):
-#     def pre_social_login(self, request, sociallogin):
-#         user = sociallogin.user
-#         if user.id:
-#             return
-#         try:
-#             customer = Customer.objects.get(email=user.email)  # if user exists, connect the account to the existing account and login
-#             sociallogin.state['process'] = 'connect'
-#             perform_login(request, customer, 'none')
-#         except Customer.DoesNotExist:
-#             pass
+from allauth.account.utils import perform_login
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+from place.models import CustomUser
+import logging
+
+logger = logging.getLogger('django')
+
+class MySocialAccountAdapter(DefaultSocialAccountAdapter):
+    def pre_social_login(self, request, sociallogin):
+        user = sociallogin.user
+        logger.warning(user)
+        if user.id:
+            return
+        try:
+            customer = CustomUser.objects.get(email=user.email)  # if user exists, connect the account to the existing account and login
+            sociallogin.state['process'] = 'connect'
+            perform_login(request, customer, 'none')
+        except CustomUser.DoesNotExist:
+            pass
