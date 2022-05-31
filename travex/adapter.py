6d76398eb0f26942e196e77ea60d0206ccc13bb6
arslion-7 <arslion@yandex.ru>
2022-05-31 15:32:38 +0500

    pre-social-login 2

--- travex/adapter.py
+++ travex/adapter.py
@@ -9,18 +9,16 @@ logger = logging.getLogger('django')
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
         user = sociallogin.user
-        logger.warning(dir(user))
-        logger.warning('--------------------')
-        # logger.warning(dir(sociallogin))
         if user.id:
             return
         try:
             custom_user = CustomUser.objects.get(email=user.email)  # if user exists, connect the account to the existing account and login
             google_picture = user.socialaccount_set.filter(provider='google')[0].extra_data['picture']
-            if google_picture:
-                if not user.image:
-                    user.image = google_picture
-                    user.save()
+            logger.warning(google_picture)
+            # if google_picture:
+            #     if not user.image:
+            #         user.image = google_picture
+            #         user.save()
             sociallogin.state['process'] = 'connect'
             perform_login(request, custom_user, 'none')
         except CustomUser.DoesNotExist:
