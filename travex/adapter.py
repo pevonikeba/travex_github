059cd9fdc3b0198e65a90538eb80e75d24918d91
arslion-7 <arslion@yandex.ru>
2022-05-31 16:53:08 +0500

    pre-social-login 15

--- travex/adapter.py
+++ travex/adapter.py
@@ -13,10 +13,10 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter):
             return
         try:
             custom_user = CustomUser.objects.get(email=user.email)  # if user exists, connect the account to the existing account and login
-            google_picture = user.socialaccount_set.filter(provider='google')[0].extra_data['picture']
-            if google_picture:
-                custom_user.image_social = google_picture
-                custom_user.save()
+            # google_picture = user.socialaccount_set.filter(provider='google')[0].extra_data['picture']
+            # if google_picture:
+            #     custom_user.image_social = google_picture
+            #     custom_user.save()
             sociallogin.state['process'] = 'connect'
             perform_login(request, custom_user, 'none')
         except CustomUser.DoesNotExist:
