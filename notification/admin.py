b96986a9b734cd958751effe184c917f79db32fa
arslion-7 <arslion@yandex.ru>
2022-06-23 21:23:55 +0500

    Notification send model and admin

--- notification/admin.py
+++ notification/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from loguru import logger
 
 from notification.fcm_manager import FCMManager
-from notification.models import UserDevice, Topic, Notification
+from notification.models import UserDevice, Topic, Notification, NotificationSend
 
 
 @admin.register(UserDevice)
@@ -25,4 +25,36 @@ class TopicAdmin(admin.ModelAdmin):
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user')
+    list_display = ('title', )
+
+
+@admin.register(NotificationSend)
+class NotificationSendAdmin(admin.ModelAdmin):
+    change_form_template = 'admin/notification/notification_send_change_form.html'
+
+    def response_change(self, request, obj: NotificationSend):
+        if "_send_notification" in request.POST:
+            notification: Notification = obj.notification
+            topic: Topic = obj.topic
+            users = obj.users
+            if topic:
+                FCMManager.send_topic_push(
+                    topic=topic.title,
+                    title=notification.title,
+                    body=notification.body
+                )
+            else:
+                if users.exists():
+                    # get tokens from user -> user_devices -> firebase_token
+                    tokens = []
+                    for user in users.all():
+                        for user_device in user.user_devices.all():
+                            tokens.append(user_device.firebase_token)
+                    if tokens:
+                        FCMManager.send_token_push(
+                            title=notification.title,
+                            msg=notification.body,
+                            tokens=tokens
+                        )
+
+        return super(NotificationSendAdmin, self).response_change(request, obj)
--- notification/models.py
+++ notification/models.py
@@ -3,7 +3,7 @@ from place.models import CustomUser
 
 
 class UserDevice(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_devices')
     device_name = models.CharField(max_length=255, unique=True, blank=True, null=True)
     firebase_token = models.TextField(blank=True, null=True)
 
@@ -13,27 +13,34 @@ class UserDevice(models.Model):
 
 class Topic(models.Model):
     title = models.CharField(max_length=255)
-    body = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    # image = models.ImageField(upload_to='images/notifications/topics/', null=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
 
 
 class Notification(models.Model):
-    IMPRESSION = 'IMP'
-    OTHER = 'OTH'
-    NOTIFICATION_TYPES = (
-        (IMPRESSION, 'Impression'),
-        (OTHER, 'Other'),
-    )
-    type = models.CharField(max_length=5, choices=NOTIFICATION_TYPES)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=255)
     body = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    # image = models.ImageField(upload_to='images/notifications/', null=True, blank=True)
+    image = models.ImageField(upload_to='images/notifications/', null=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
+
+
+class NotificationSend(models.Model):
+    notification = models.ForeignKey(Notification, on_delete=models.PROTECT)
+    topic = models.ForeignKey(Topic, on_delete=models.PROTECT, null=True, blank=True)
+    users = models.ManyToManyField(CustomUser, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.notification} - {self.topic} - {self.created_at}'
+
+ # IMPRESSION = 'IMP'
+ #    OTHER = 'OTH'
+ #    NOTIFICATION_TYPES = (
+ #        (IMPRESSION, 'Impression'),
+ #        (OTHER, 'Other'),
+ #    )
+ #    type = models.CharField(max_length=5, choices=NOTIFICATION_TYPES)
+# user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
